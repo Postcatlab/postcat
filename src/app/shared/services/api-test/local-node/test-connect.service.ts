6ed46b8c0996b7e99895df967d688344434e3425
kungfuboy <17kungfuboy@gmail.com>
2022-01-17 22:07:35 +0800

    Fix the problem that the env can‘t be switched

--- src/app/shared/services/api-test/local-node/test-connect.service.ts
+++ src/app/shared/services/api-test/local-node/test-connect.service.ts
@@ -123,6 +123,7 @@ export class TestServerLocalNodeService implements TestServer {
       return result;
     };
     const formatEnv = (env) => {
+      console.log('env.parameters', env);
       let result = {
         paramList: env.parameters.map((val) => ({ paramKey: val.name, paramValue: val.value })),
         frontURI: env.hostUri,
