a7734d5b8256980774c92e860d784827a513ff38
秦圆圆 <scarqin@gmail.com>
2022-01-17 19:56:06 +0800

    style and fix some bug

--- angular.webpack.js
+++ angular.webpack.js
@@ -1,45 +1,29 @@
 //Polyfill Node.js core modules in Webpack. This module is only needed for webpack 5+.
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 /**
  * Custom angular webpack configuration
  */
 module.exports = (config, options) => {
-  config.target = "electron-renderer";
+  config.target = 'electron-renderer';
 
   if (options.fileReplacements) {
     for (let fileReplacement of options.fileReplacements) {
-      if (fileReplacement.replace !== "src/environments/environment.ts") {
+      if (fileReplacement.replace !== 'src/environments/environment.ts') {
         continue;
       }
 
-      let fileReplacementParts = fileReplacement["with"].split(".");
-      if (
-        fileReplacementParts.length > 1 &&
-        ["web"].indexOf(fileReplacementParts[1]) >= 0
-      ) {
-        config.target = "web";
+      let fileReplacementParts = fileReplacement['with'].split('.');
+      if (fileReplacementParts.length > 1 && ['web'].indexOf(fileReplacementParts[1]) >= 0) {
+        config.target = 'web';
       }
       break;
     }
   }
-  if(config.module&&config.module.rules){
-    config.module.rules.push( {
-      test: /\.html$/i,
-      use: [
-        {
-          loader: 'raw-loader',
-          options: {
-            esModule: false,
-          }
-        },
-      ],
-    },)
-  }
   config.plugins = [
     ...config.plugins,
     new NodePolyfillPlugin({
-      excludeAliases: ["console"],
+      excludeAliases: ['console'],
     }),
   ];
 
