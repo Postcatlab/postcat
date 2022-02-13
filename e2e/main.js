cf96c300a72375b22b1f4085206be566822af08a
夜鹰 <17kungfuboy@gmail.com>
2022-02-14 07:39:36 +0800

    support run e2e in npm scripts

--- e2e/main.js
+++ e2e/main.js
@@ -1,11 +1,18 @@
 const fs = require('fs');
+const path = require('path');
 const _ = require('lodash');
 
 function main(data) {
   // console.log(JSON.stringify(data, null, 2));
   data.forEach(({ block, cases }) => {
     cases.forEach((item) => {
-      fs.writeFileSync(`./test/${_.snakeCase(block.title + ' ' + item.title)}.t`, item.content);
+      const _path = path.join(__dirname, `./cases/${_.snakeCase(block.title + ' ' + item.title)}.t`);
+      fs.writeFile(_path, item.content, (err) => {
+        if (err) {
+          return console.log(err);
+        }
+        console.log(`The file was saved: ${_path}`);
+      });
     });
   });
 }
