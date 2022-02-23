8e44962d7b2a8c00b9dcc9e9e43bebd8dd4dcf4f
秦圆圆 <scarqin@gmail.com>
2022-02-23 19:05:20 +0800

    stash

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ElectronService } from '../../../core/services';
+import { shell } from 'electron';
 @Component({
   selector: 'eo-navbar',
   templateUrl: './navbar.component.html',
@@ -11,6 +12,9 @@ export class NavbarComponent implements OnInit {
   OS_TYPE = navigator.platform.toLowerCase();
   constructor(private electron: ElectronService) {
     this.isElectron = this.electron.isElectron;
+  }
+  openLink(link) {
+    shell.openExternal(link);
   }
   minimize() {
     this.electron.ipcRenderer.send('message', {
