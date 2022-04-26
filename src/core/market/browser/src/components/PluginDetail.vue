14d1a608642866f93ed7ca3c5aa2e41237ccffa3
夜鹰 <17kungfuboy@gmail.com>
2022-04-26 18:28:54 +0800

    feature: Filter out plugins whose authors are eolink

--- src/core/market/browser/src/components/PluginDetail.vue
+++ src/core/market/browser/src/components/PluginDetail.vue
@@ -5,23 +5,23 @@
   <section class="">
     <div class="flex p-8">
       <i
-        class="block h-40 w-40 bg-cover bg-center bg-no-repeat mr-8"
+        class="block border rounded-lg h-40 w-40 bg-cover bg-center bg-no-repeat mr-8"
         :style="{ backgroundImage: `url(${pluginDetail.logo || ''})` }"
       ></i>
       <div class="flex flex-col flex-1">
         <div class="flex flex-col">
-          <span class="text-lg mb-2">{{ pluginDetail.name }}</span>
+          <span class="text-xl mb-2 font-bold">{{ pluginDetail.name }}</span>
           <span>作者: {{ pluginDetail.author }}</span>
-          <!-- <span class="mb-4">Tags: {Tags}</span> -->
-          <span class="mb-4">Version: {{ pluginDetail.version }}</span>
+          <!-- <span class="mb-4">Tags: {{ pluginDetail.keywords }}</span> -->
+          <span class="mb-2">Version: {{ pluginDetail.version }}</span>
           <p class="w-full h-20">{{ pluginDetail.description }}</p>
         </div>
         <div class="flex">
           <div class="flex items-center" v-if="!pluginDetail.installed">
-            <a-button type="primary mr-4" size="large" @click="installApp(pluginDetail.name)">安装</a-button>
-            <span>安装完成后需要重启</span>
+            <a-button type="primary mr-4" @click="installApp(pluginDetail.name)">安装</a-button>
+            <!-- <span class="text-gray-500">安装完成后需要重启</span> -->
           </div>
-          <a-button v-else type="primary" size="large" @click="uninstallApp(pluginDetail.name)" danger>卸载</a-button>
+          <a-button v-else type="primary" @click="uninstallApp(pluginDetail.name)" danger>卸载</a-button>
         </div>
       </div>
     </div>
@@ -30,20 +30,21 @@
         <a-tab-pane key="desc" tab="概述"> {{ pluginDetail.description }} </a-tab-pane>
         <a-tab-pane key="more" tab="更多信息"> Content of Tab Pane 2 </a-tab-pane>
         <a-tab-pane key="setting" tab="设置" v-if="pluginDetail.configuration && pluginDetail.configuration.properties">
-        <a-form name="basic" :label-col="{ span: 4 }" :wrapper-col="{ span: 20 }" autocomplete="off">
-          <a-form-item v-for="(item, field) in pluginDetail.configuration.properties"
-            :label="item.label"
-            :name="field"
-            :key="field"
-            :rules="[{ required: item.required }]"
-          >
-            <a-input />
-          </a-form-item>
+          <a-form name="basic" :label-col="{ span: 4 }" :wrapper-col="{ span: 20 }" autocomplete="off">
+            <a-form-item
+              v-for="(item, field) in pluginDetail.configuration.properties"
+              :label="item.label"
+              :name="field"
+              :key="field"
+              :rules="[{ required: item.required }]"
+            >
+              <a-input />
+            </a-form-item>
 
-          <a-form-item :wrapper-col="{ offset: 4, span: 20 }">
-            <a-button type="primary" html-type="submit">Submit</a-button>
-          </a-form-item>
-        </a-form>
+            <a-form-item :wrapper-col="{ offset: 4, span: 20 }">
+              <a-button type="primary" html-type="submit">Submit</a-button>
+            </a-form-item>
+          </a-form>
         </a-tab-pane>
       </a-tabs>
     </div>
@@ -71,6 +72,7 @@ const localModules = store.getLocalModules;
 const installApp = (name) => {
   console.log('Install module:', name);
   const { code, data, modules } = window.eo.installModule(name);
+  console.log('Done', code);
   if (code === 0) {
     pluginDetail.installed = true;
     store.updateLocalModules(modules);
@@ -101,7 +103,7 @@ onMounted(async () => {
   }
   if (localModules.has(moduleID)) {
     Object.assign(pluginDetail, localModules.get(moduleID), { installed: true });
-  } 
+  }
   const [data, err] = await getDetail(name || '');
   if (err) {
     return;
@@ -111,7 +113,7 @@ onMounted(async () => {
 </script>
 <style lang="less" scoped>
 .ant-btn-dangerous.ant-btn-primary {
-  background: #ff3c32;
+  background: #ff3d33;
 }
 </style>
 <style>
