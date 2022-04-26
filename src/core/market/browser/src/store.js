1b4390d2d07c13908629872dbb0087bc8fa6cbbf
everright <everright.chen@gmail.com>
2022-04-26 11:23:18 +0800

    fix: module install & uninstall

--- src/core/market/browser/src/store.js
+++ src/core/market/browser/src/store.js
@@ -5,10 +5,12 @@ export const useStore = defineStore({
   // id: 必须，在所有 Store 中唯一
   id: 'globalState',
   // state: 返回对象的函数
-  state: () => ({
-    pluginList: [],
-    localModules: window.eo.getModules()
-  }),
+  state: () => {
+    return {
+      localModules: window.eo.getModules(),
+      pluginList: [],
+    }
+  },
   // getter 第一个参数是 state，是当前的状态，也可以使用 this 获取状态
   // getter 中也可以访问其他的 getter，或者是其他的 Store
   getters: {
@@ -20,6 +22,9 @@ export const useStore = defineStore({
     updatePluginList(list) {
       // 使用 this 直接修改
       this.pluginList = Array.from(list.keys()).filter((it) => it);
+    },
+    updateLocalModules(modules) {
+      this.localModules = modules;
     },
   },
 });
