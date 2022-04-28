00585eb7e373a3de081ff894c56403871f926c24
夜鹰 <17kungfuboy@gmail.com>
2022-04-28 16:35:36 +0800

    feat: test sync_to_remote

--- src/workbench/browser/src/app/shared/components/sync-api/sync-api.component.ts
+++ src/workbench/browser/src/app/shared/components/sync-api/sync-api.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NzModalRef } from 'ng-zorro-antd/modal';
 import { StorageService } from '../../services/storage';
+import { sync_to_remote } from './push.js';
 
 @Component({
   selector: 'eo-sync-api',
@@ -24,7 +25,15 @@ export class SyncApiComponent implements OnInit {
         title: it.features['apimanager.sync'].label,
       }));
   }
-  submit() {
+  async submit() {
     console.log('sync');
+    // const res = await sync_to_remote(
+    //   { a: 1 },
+    //   {
+    //     projectId: 'ZXXhzTG756db7e34a8d7c803f001edf1a1c545bcbf27719',
+    //     SecretKey: 'SgAZ5Lk60f776c1a235a3c3e62543c3793c36d6cc511db9',
+    //   }
+    // );
+    // console.log(res);
   }
 }
