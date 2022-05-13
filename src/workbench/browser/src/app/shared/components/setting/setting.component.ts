ced540449f73221d7a10e042fd755ddd68d9fc2d
bqy_fe <1743369777@qq.com>
2022-05-13 09:05:25 +0800

    remove redundant code

--- src/workbench/browser/src/app/shared/components/setting/setting.component.ts
+++ src/workbench/browser/src/app/shared/components/setting/setting.component.ts
@@ -28,6 +28,7 @@ interface FlatNode {
   styleUrls: ['./setting.component.scss'],
 })
 export class SettingComponent implements OnInit {
+  objectKeys = Object.keys;
   private transformer = (node: TreeNode, level: number): FlatNode => ({
     ...node,
     expandable: !!node.children && node.children.length > 0,
@@ -56,10 +57,10 @@ export class SettingComponent implements OnInit {
   isVisible = false;
   isModal = false;
   position: NzTabPosition = 'left';
-  /** 所有模块 */
-  modules = [];
   /** 所有配置 */
   settings = {};
+  /** 深层嵌套的配置 */
+  nestedSettings = {};
   validateForm!: FormGroup;
   constructor(private fb: FormBuilder) {}
 
@@ -76,15 +77,7 @@ export class SettingComponent implements OnInit {
    * @param properties
    */
   private setSettingsModel(properties, controls) {
-    // Object.keys(properties).forEach((key) => {
-    //   const keyArr = key.split('.');
-    //   const keyArrL = keyArr.length - 1;
-    //   keyArr.reduce((p, k, i) => {
-    //     const isLast = i === keyArrL;
-    //     p[k] ??= isLast ? properties[key].default : {};
-    //     return p[k];
-    //   }, this.settings);
-    // });
+    //  平级配置对象
     Object.keys(properties).forEach((fieldKey) => {
       const props = properties[fieldKey];
       this.settings[fieldKey] = props.default;
@@ -95,6 +88,26 @@ export class SettingComponent implements OnInit {
         controls[fieldKey] = [null];
       }
     });
+    // 深层嵌套的配置对象
+    Object.keys(properties).forEach((fieldKey) => {
+      const keyArr = fieldKey.split('.');
+      const keyArrL = keyArr.length - 1;
+      keyArr.reduce((p, k, i) => {
+        const isLast = i === keyArrL;
+        p[k] ??= isLast ? properties[fieldKey]?.default : {};
+        return p[k];
+      }, this.nestedSettings);
+      // 当settings变化时，将值同步到nestedSettings
+      Object.defineProperty(this.settings, fieldKey, {
+        get: () => {
+          return this.getConfiguration(fieldKey);
+        },
+        set: (newVal) => {
+          const target = keyArr.slice(0, -1).reduce((p, k) => p[k], this.nestedSettings);
+          target[keyArr[keyArrL]] = newVal;
+        },
+      });
+    });
   }
 
   /**
@@ -103,7 +116,7 @@ export class SettingComponent implements OnInit {
    * @returns
    */
   getConfiguration(key: string) {
-    return key.split('.').reduce((p, k) => p[k], this.settings);
+    return key.split('.').reduce((p, k) => p[k], this.nestedSettings);
   }
   /**
    * 获取模块的标题
@@ -116,10 +129,10 @@ export class SettingComponent implements OnInit {
   }
 
   selectModule(node) {
-    console.log('selectModule', node);
+    // console.log('selectModule', node);
     this.currentConfiguration = node.configuration || [];
-    console.log('this.currentConfiguration', this.currentConfiguration);
-    this.selectListSelection.toggle(node);
+    // console.log('this.currentConfiguration', this.currentConfiguration);
+    this.selectListSelection.select(node);
   }
 
   /**
@@ -147,11 +160,10 @@ export class SettingComponent implements OnInit {
         this.setSettingsModel(item.properties, controls);
       }
     });
-
-    const generateTreeData = (
-      configurations: typeof allConfiguration[number] | Array<typeof allConfiguration[number]> = allConfiguration
-    ) => {
-      return (Array.isArray(configurations) ? configurations : [configurations]).reduce<TreeNode[]>((prev, curr) => {
+    type Configuration = typeof allConfiguration[number] | Array<typeof allConfiguration[number]>;
+    // 递归生成设置树
+    const generateTreeData = (configurations: Configuration = []) => {
+      return [].concat(configurations).reduce<TreeNode[]>((prev, curr) => {
         if (Array.isArray(curr)) {
           return prev.concat(generateTreeData(curr));
         }
@@ -163,6 +175,7 @@ export class SettingComponent implements OnInit {
         return prev.concat(treeItem);
       }, []);
     };
+    // 所有设置项
     const treeData = allSettings.reduce<TreeNode[]>((prev, curr) => {
       let treeItem: TreeNode;
       const configuration = curr.contributes.configuration;
@@ -184,67 +197,70 @@ export class SettingComponent implements OnInit {
     }, []);
     console.log('treeData', treeData);
     console.log('setings', this.settings);
+    console.log('nestedSettings', this.nestedSettings);
     console.log('controls', controls);
-    this.modules = allSettings;
+
     this.dataSource.setData(treeData);
     this.treeControl.expandAll();
     this.validateForm = this.fb.group(controls);
+    // 默认选中第一项
+    this.selectModule(this.treeControl.dataNodes.at(0));
   }
 
-  private init() {
-    if (window.eo && window.eo.getFeature) {
-      this.isVisible = true;
-      this.settings = window.eo.getSettings();
-      const featureList = window.eo.getFeature('configuration');
-      const controls = {};
-      featureList?.forEach((feature: object, key: string) => {
-        if (!feature['title'] || !feature['properties'] || typeof feature['properties'] !== 'object') {
-          return true;
-        }
-        if (!this.settings[key] || typeof this.settings[key] !== 'object') {
-          this.settings[key] = {};
-        }
-        const fields = [];
-        for (let field_key in feature['properties']) {
-          let field = feature['properties'][field_key];
-          // 加入允许的type限制
-          if (!field['type'] || !field['label']) {
-            continue;
-          }
-          if ('select' === field['type'] && !field['options']) {
-            continue;
-          }
-          const name = key + '_' + field_key;
-          field = Object.assign(
-            {
-              name: name,
-              key: field_key,
-              required: false,
-              default: '',
-              description: '',
-            },
-            field
-          );
-          fields.push(field);
-          if (!this.settings[key][field_key]) {
-            this.settings[key][field_key] = field['default'];
-          }
-          // 可扩展加入更多默认校验
-          if (field.required) {
-            controls[name] = [null, [Validators.required]];
-          } else {
-            controls[name] = [null];
-          }
-        }
-        this.modules.push({
-          key: key,
-          title: feature['title'],
-          fields: fields,
-        });
-      });
-      this.validateForm = this.fb.group(controls);
-    }
-  }
+  // private init() {
+  //   if (window.eo && window.eo.getFeature) {
+  //     this.isVisible = true;
+  //     this.settings = window.eo.getSettings();
+  //     const featureList = window.eo.getFeature('configuration');
+  //     const controls = {};
+  //     featureList?.forEach((feature: object, key: string) => {
+  //       if (!feature['title'] || !feature['properties'] || typeof feature['properties'] !== 'object') {
+  //         return true;
+  //       }
+  //       if (!this.settings[key] || typeof this.settings[key] !== 'object') {
+  //         this.settings[key] = {};
+  //       }
+  //       const fields = [];
+  //       for (let field_key in feature['properties']) {
+  //         let field = feature['properties'][field_key];
+  //         // 加入允许的type限制
+  //         if (!field['type'] || !field['label']) {
+  //           continue;
+  //         }
+  //         if ('select' === field['type'] && !field['options']) {
+  //           continue;
+  //         }
+  //         const name = key + '_' + field_key;
+  //         field = Object.assign(
+  //           {
+  //             name: name,
+  //             key: field_key,
+  //             required: false,
+  //             default: '',
+  //             description: '',
+  //           },
+  //           field
+  //         );
+  //         fields.push(field);
+  //         if (!this.settings[key][field_key]) {
+  //           this.settings[key][field_key] = field['default'];
+  //         }
+  //         // 可扩展加入更多默认校验
+  //         if (field.required) {
+  //           controls[name] = [null, [Validators.required]];
+  //         } else {
+  //           controls[name] = [null];
+  //         }
+  //       }
+  //       // this.modules.push({
+  //       //   key: key,
+  //       //   title: feature['title'],
+  //       //   fields: fields,
+  //       // });
+  //     });
+  //     this.validateForm = this.fb.group(controls);
+  //   }
+  // }
 
   handleShowModal() {
     this.isModal = true;
