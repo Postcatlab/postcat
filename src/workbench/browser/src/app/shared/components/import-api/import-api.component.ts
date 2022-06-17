6e5afef4dbc6b4b7603a3be753c0d11243695b44
夜鹰 <17kungfuboy@gmail.com>
2022-06-17 17:20:28 +0800

    Refactor/import (#75)
    * feat: delete useless code
    
    * feat: projectExport error
    
    * feat: delete useless code
    
    * feat: projectExport error
    
    * feat: update code

--- src/workbench/browser/src/app/shared/components/import-api/import-api.component.ts
+++ src/workbench/browser/src/app/shared/components/import-api/import-api.component.ts
@@ -1,17 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { StorageRes, StorageResStatus } from 'eo/workbench/browser/src/app/shared/services/storage/index.model';
-import { EoMessageService } from 'eo/workbench/browser/src/app/eoui/message/eo-message.service';
-import { StorageService } from '../../../shared/services/storage';
 import { FeatureType } from '../../types';
-import { parserProperties, getDefaultValue } from '../../../utils';
 import { MessageService } from 'eo/workbench/browser/src/app/shared/services/message/message.service';
+
+// const optionList = [
+//   {
+//     value: 'import',
+//     type: 'string',
+//     default: '',
+//     label: '直接导入',
+//     description: '直接导入',
+//   },
+//   {
+//     value: 'add',
+//     type: 'string',
+//     default: true,
+//     label: '增量更新[推荐]',
+//     description: '增量更新',
+//   },
+//   {
+//     value: 'all',
+//     type: 'string',
+//     default: '',
+//     label: '全量更新[慎用]',
+//     description: '全量更新',
+//   },
+//   {
+//     value: 'new',
+//     type: 'string',
+//     default: '',
+//     label: '仅添加新 API',
+//     description: '仅添加新 API',
+//   },
+// ];
 
 @Component({
   selector: 'eo-import-api',
   template: `<extension-select
     [allowDrag]="true"
-    [optionList]="optionList"
-    [(currentOption)]="currentOption"
     [(extension)]="currentExtension"
     [extensionList]="supportList"
     (uploadChange)="uploadChange($event)"
@@ -20,15 +45,9 @@ import { MessageService } from 'eo/workbench/browser/src/app/shared/services/mes
 export class ImportApiComponent implements OnInit {
   supportList: Array<FeatureType> = [];
   currentExtension = '';
-  currentOption = '';
-  optionList = [];
   uploadData = null;
   featureMap = window.eo.getFeature('apimanage.import');
-  constructor(
-    private storage: StorageService,
-    private message: EoMessageService,
-    private messageService: MessageService
-  ) {}
+  constructor(private messageService: MessageService) {}
   ngOnInit(): void {
     this.featureMap?.forEach((data: FeatureType, key: string) => {
       this.supportList.push({
@@ -37,39 +56,25 @@ export class ImportApiComponent implements OnInit {
       });
     });
     {
-      const { key, properties } = this.supportList.at(0);
+      const { key } = this.supportList.at(0);
       this.currentExtension = key || '';
-      this.optionList = parserProperties(properties || '');
-      this.currentOption = getDefaultValue(this.optionList, 'value');
     }
-  }
-  getEoapiData() {
-    return new Promise((resolve) => {
-      this.storage.run('projectExport', [], (result: StorageRes) => {
-        const isOk = result.status === StorageResStatus.success;
-        resolve(isOk ? [result.data, null] : [null, true]);
-      });
-    });
   }
   uploadChange(data) {
     this.uploadData = data;
   }
   async submit(callback) {
-    const [eoapiData, err]: any = await this.getEoapiData();
-    if (err) {
-      this.message.error('获取本地数据失败');
-      return;
-    }
     // * this.currentExtension is extension's key, like 'eoapi-import-openapi'
     const feature = this.featureMap.get(this.currentExtension);
     const action = feature.action || null;
     const module = window.eo.loadFeatureModule(this.currentExtension);
-    const data = module[action](eoapiData, this.uploadData, this.currentOption);
+    const { name, content } = this.uploadData;
+    const data = module[action](content);
+    // console.log(JSON.stringify(data, null, 2));
     this.messageService.send({
       type: 'importSuccess',
-      data: JSON.stringify(data),
+      data: { name, content: data },
     });
-    // console.log(JSON.stringify(data));
     callback(true);
   }
 }
