930ddae3c2e3e1668bc32bba9ac22bcd140f8e51
scarqin <1054139596@qq.com>
2022-06-23 22:29:36 +0800

    feat: i18n

--- src/workbench/browser/src/polyfills.ts
+++ src/workbench/browser/src/polyfills.ts
@@ -58,3 +58,4 @@ import 'zone.js'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import '@angular/localize/init';
