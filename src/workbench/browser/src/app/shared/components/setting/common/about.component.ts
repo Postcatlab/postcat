dbe7ba09ca03e7d5fb75559de7736bea671fa8b8
buqiyuan <1743369777@qq.com>
2022-06-27 18:50:18 +0800

    fix: some error message

--- src/workbench/browser/src/app/shared/components/setting/common/about.component.ts
+++ src/workbench/browser/src/app/shared/components/setting/common/about.component.ts
@@ -83,28 +83,28 @@ export class AboutComponent implements OnInit {
   constructor(private electron: ElectronService) {}
 
   ngOnInit(): void {
-    fetch('https://api.github.com/repos/eolinker/eoapi/releases')
-      .then((response) => response.json())
-      .then((data) => {
-        const publishTime = data.find((n) => n.tag_name.slice(1) === pkg.version)?.published_at;
-        const publishObj = this.list.find((n) => n.id === 'publishTime');
-        if (publishTime) {
-          publishObj.value = new Intl.DateTimeFormat('zh-CN', {
-            year: 'numeric',
-            month: '2-digit',
-            weekday: 'long',
-            day: '2-digit',
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit',
-            hour12: false,
-          })
-            .format(new Date(publishTime))
-            .replace(/星期[^]?/, '');
-        } else {
-          publishObj.value = `当前版本(v${pkg.version})尚未发布`;
-        }
-      });
+    // fetch('https://api.github.com/repos/eolinker/eoapi/releases')
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     const publishTime = data.find((n) => n.tag_name.slice(1) === pkg.version)?.published_at;
+    //     const publishObj = this.list.find((n) => n.id === 'publishTime');
+    //     if (publishTime) {
+    //       publishObj.value = new Intl.DateTimeFormat('zh-CN', {
+    //         year: 'numeric',
+    //         month: '2-digit',
+    //         weekday: 'long',
+    //         day: '2-digit',
+    //         hour: '2-digit',
+    //         minute: '2-digit',
+    //         second: '2-digit',
+    //         hour12: false,
+    //       })
+    //         .format(new Date(publishTime))
+    //         .replace(/星期[^]?/, '');
+    //     } else {
+    //       publishObj.value = `当前版本(v${pkg.version})尚未发布`;
+    //     }
+    //   });
     const systemInfo = this.getSystemInfo();
     this.list.forEach((item) => {
       if (item.id in systemInfo) {
--- src/workbench/browser/src/app/shared/components/setting/common/data-storage.component.ts
+++ src/workbench/browser/src/app/shared/components/setting/common/data-storage.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'eo-data-storage',
@@ -24,20 +25,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
       <ng-container *ngIf="validateForm.value.dataStorage === 'remote-server'">
         <nz-form-item>
           <nz-form-label>Host</nz-form-label>
-          <nz-form-control nzErrorTip="Please input your Host!">
+          <nz-form-control nzErrorTip="Please input your Host">
             <input nz-input formControlName="remoteServer.url" placeholder="your host" />
           </nz-form-control>
         </nz-form-item>
         <nz-form-item>
           <nz-form-label>Security Token</nz-form-label>
-          <nz-form-control nzErrorTip="Please input your Security Token!">
+          <nz-form-control nzErrorTip="Please input your Security Token">
             <input nz-input formControlName="remoteServer.token" placeholder="your security token" />
           </nz-form-control>
         </nz-form-item>
       </ng-container>
       <nz-form-item>
         <nz-form-control>
-          <button nz-button nzType="primary">Change Data Storage</button>
+          <button nz-button nzType="primary" [nzLoading]="loading">Change Data Storage</button>
         </nz-form-control>
       </nz-form-item>
     </form>
@@ -51,13 +52,71 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   ],
 })
 export class DataStorageComponent implements OnInit, OnChanges {
-  @Input() model: object;
+  @Input() model: Record<string, any> = {};
   @Output() modelChange: EventEmitter<any> = new EventEmitter();
   validateForm!: FormGroup;
+  loading = false;
 
-  submitForm(): void {
+  constructor(private fb: FormBuilder, private message: NzMessageService) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      dataStorage: this.model.dataStorage ?? 'remote-server',
+      'remoteServer.url': [this.model['remoteServer.url'], [Validators.required]],
+      'remoteServer.token': [this.model['remoteServer.token'], [Validators.required]],
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const { model } = changes;
+
+    if (model && this.validateForm?.value) {
+      this.setFormValue(model.currentValue);
+    }
+  }
+
+  /**
+   * 测试远程服务器地址是否可用
+   */
+  async pingRmoteServerUrl() {
+    const dataStorage = this.validateForm.value.dataStorage;
+    const remoteUrl = this.validateForm.value['remoteServer.url'];
+    const token = this.validateForm.value['remoteServer.token'];
+
+    if (dataStorage !== 'remote-server') {
+      return Promise.reject(false);
+    }
+
+    try {
+      const url = `${remoteUrl}/system/status`.replace(/(?<!:)\/{2,}/g, '/');
+      const response = await fetch(url, {
+        headers: {
+          'x-api-key': token,
+        },
+      });
+      const result = await response.json();
+      console.log('result', result);
+      if (result.statusCode !== 200) {
+        throw result;
+      }
+      // await result.json();
+      this.message.create('success', '远程服务器地址设置成功！');
+      return Promise.resolve(true);
+    } catch (error) {
+      console.error(error);
+      this.message.create('error', '远程服务器连接失败！');
+      return Promise.reject(false);
+    }
+  }
+
+  async submitForm() {
     if (this.validateForm.valid) {
       console.log('submit', this.validateForm.value);
+      this.loading = true;
+      const result = await this.pingRmoteServerUrl().finally(() => (this.loading = false));
+      if (Object.is(result, true)) {
+        this.message.success('远程数据源连接成功');
+      }
       this.model = this.validateForm.value;
       this.modelChange.emit(this.validateForm.value);
     } else {
@@ -70,20 +129,9 @@ export class DataStorageComponent implements OnInit, OnChanges {
     }
   }
 
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit(): void {
-    this.validateForm = this.fb.group({
-      dataStorage: 'remote-server',
-      'remoteServer.url': [null, [Validators.required]],
-      'remoteServer.token': [null, [Validators.required]],
+  setFormValue(model = {}) {
+    Object.keys(model).forEach((key) => {
+      this.validateForm.get(key)?.setValue(model[key]);
     });
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    const { model } = changes;
-    if (model) {
-      Object.assign(this.validateForm.value, model.currentValue);
-    }
   }
 }
