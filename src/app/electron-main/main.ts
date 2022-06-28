6fe65565650eb3d3acdcaafe4c0fd0a221011867
scarqin <1054139596@qq.com>
2022-06-28 18:01:06 +0800

    chore: build i18n destop app

--- src/app/electron-main/main.ts
+++ src/app/electron-main/main.ts
@@ -59,7 +59,7 @@ function createWindow(): BrowserWindow {
     const file: string =
       processEnv === 'development'
         ? 'http://localhost:4200'
-        : `file://${path.join(__dirname, '../../../src/workbench/browser/dist/index.html')}`;
+        : `file://${path.join(__dirname, '../../../src/workbench/browser/dist/zh-Hans/index.html')}`;
     win.loadURL(file);
     if (['development'].includes(processEnv)) {
       win.webContents.openDevTools({
