11f74700f8c3ed05a225afc11fe321493d39207d
buqiyuan <1743369777@qq.com>
2022-07-01 18:55:10 +0800

    fix: some css style issues

--- src/workbench/browser/src/app/eoui/iconpark-icon/eo-iconpark-icon.component.ts
+++ src/workbench/browser/src/app/eoui/iconpark-icon/eo-iconpark-icon.component.ts
@@ -3,11 +3,15 @@ import { Component, Input } from '@angular/core';
 @Component({
   // standalone: true,
   selector: 'eo-iconpark-icon',
-  template: `<iconpark-icon [name]="name"></iconpark-icon>`,
+  template: `<iconpark-icon [name]="name" [ngStyle]="{ fontSize: size }"></iconpark-icon>`,
   styleUrls: [],
+  host: {
+    class: 'inline-flex',
+  },
 })
 export class EoIconparkIconComponent {
   @Input() name: string;
+  @Input() size = '18px';
 
   constructor() {}
 }
