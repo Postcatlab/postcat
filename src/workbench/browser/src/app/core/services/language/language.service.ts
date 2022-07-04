1617f56c815117954dde8ad02b9822e64774016d
scarqin <1054139596@qq.com>
2022-07-04 11:58:08 +0800

    feat: changeLanuage

---
+++ src/workbench/browser/src/app/core/services/language/language.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { RemoteService } from 'eo/workbench/browser/src/app/shared/services/remote/remote.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LanguageService {
+  currentLanguage = navigator.language.includes('zh') ? 'zh-Hans' : 'en-US';
+  languages = [
+    {
+      name: 'English',
+      value: 'en-US',
+      path: 'en',
+    },
+    {
+      name: '简体中文',
+      value: 'zh-Hans',
+      path: 'zh',
+    },
+  ];
+  constructor(private remote: RemoteService) {
+    console.log('LanguageService')
+  }
+  init(){
+    const configLanguage = this.remote.getSettings()?.['eoapi-language'];
+    this.changeLanguage(configLanguage);
+  }
+  changeLanguage(localeID) {
+    if (localeID === this.currentLanguage) {
+      console.warn(`current language has already ${localeID}`);
+      return;
+    }
+    this.currentLanguage = localeID;
+    window.location.href = `/${(this.languages.find((val) => val.value === localeID) || this.languages[0]).path}`;
+  }
+}
