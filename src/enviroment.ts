0f10f0aa65e7fbbf29fbf6a8be4be9936c443698
scarqin <1054139596@qq.com>
2022-07-21 08:37:47 +0800

    fix: demo test error

--- src/enviroment.ts
+++ src/enviroment.ts
@@ -1,4 +1,5 @@
 export const ELETRON_APP_CONFIG = {
   EXTENSION_URL: 'http://106.12.149.147',
-  MOCK_URL: 'https://mockapi.eolink.com/ztBFKai20ee60c12871881565b5a6ddd718337df0e30979'
+  MOCK_URL: 'https://mockapi.eolink.com/ztBFKai20ee60c12871881565b5a6ddd718337df0e30979',
+  NODE_SERVER_PORT:4201
 };
