7cac8f335a5455b1a5c3ba4454729ac427d89201
buqiyuan <1743369777@qq.com>
2022-08-01 18:36:08 +0800

    fix: ts error

--- src/workbench/browser/src/typings.d.ts
+++ src/workbench/browser/src/typings.d.ts
@@ -5,17 +5,15 @@ import * as monaco from 'monaco-editor';
 interface NodeModule {
   id: string;
 }
-declare global {
-  interface Window {
-    process: any;
-    requirejs: any;
-    require: any;
-    angular: any;
-    eo: any;
-    BlobBuilder: any;
-    WebKitBlobBuilder: any;
-    MozBlobBuilder: any;
-    MSBlobBuilder: any;
-    monaco: typeof monaco;
-  }
+interface Window {
+  process: any;
+  requirejs: any;
+  require: any;
+  angular: any;
+  eo: any;
+  BlobBuilder: any;
+  WebKitBlobBuilder: any;
+  MozBlobBuilder: any;
+  MSBlobBuilder: any;
+  monaco: typeof monaco;
 }
