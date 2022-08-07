c3849d35811c3a8cee7ecb18168e0eb5c6131ad1
kungfuboy <17kungfuboy@gmail.com>
2022-08-08 07:40:27 +0800

    feat: update

---
+++ src/workbench/browser/src/app/shared/services/storage/local.db.ts
@@ -0,0 +1,154 @@
+import Dexie, { Table } from 'dexie';
+import { getSettings } from 'eo/workbench/browser/src/app/core/services/settings/settings.service';
+import { messageService } from 'eo/workbench/browser/src/app/shared/services/message/message.service';
+import { DataSourceType } from 'eo/workbench/browser/src/app/shared/services/storage/storage.service';
+import { tree2obj } from 'eo/workbench/browser/src/app/utils/tree/tree.utils';
+import {
+  Project,
+  Environment,
+  Group,
+  ApiData,
+  ApiTestHistory,
+  ApiMockEntity,
+  StorageInterface,
+  StorageItem,
+  StorageResStatus,
+} from './index.model';
+import { sampleApiData } from './IndexedDB/sample';
+
+export type ResultType<T = any> = {
+  status: StorageResStatus.success;
+  data: T;
+};
+
+let isFirstLoad = true;
+const getApiUrl = (apiData: ApiData) => {
+  const dataSourceType: DataSourceType = getSettings()?.['eoapi-common.dataStorage'] ?? 'local';
+
+  /** Is it a remote data source */
+  const isRemote = dataSourceType === 'http';
+
+  /** get mock url */
+  const mockUrl = isRemote
+    ? window.eo?.getModuleSettings?.('eoapi-common.remoteServer.url') + '/mock/eo-1/'
+    : window.eo?.getMockUrl?.();
+
+  const url = new URL(`${mockUrl}/${apiData.uri}`.replace(/(?<!:)\/{2,}/g, '/'), 'https://github.com/');
+  // if (apiData) {
+  //   url.searchParams.set('mockID', apiData.uuid + '');
+  // }
+  return decodeURIComponent(url.toString());
+};
+
+const createMockObj = (apiData: ApiData, options: Record<string, any> = {}) => {
+  const { name = '', createWay = 'custom', ...rest } = options;
+  return {
+    name,
+    url: getApiUrl(apiData),
+    apiDataID: apiData.uuid,
+    projectID: 1,
+    createWay,
+    response: JSON.stringify(tree2obj([].concat(apiData.responseBody))),
+    ...rest,
+  };
+};
+
+const batchCreateMock = async (mock: Table<ApiMockEntity, number | string>, data: ApiData[]) => {
+  try {
+    if (Array.isArray(data)) {
+      isFirstLoad = false;
+      const mockList = await mock.where('uuid').above(0).toArray();
+      const noHasDefaultMockApiDatas = data
+        .filter((item) => !mockList.some((m) => Number(m.apiDataID) === item.uuid))
+        .map((item) => createMockObj(item, { name: $localize`Default Mock`, createWay: 'system' }));
+
+      await mock.bulkAdd(noHasDefaultMockApiDatas);
+    }
+    messageService.send({ type: 'mockAutoSyncSuccess', data: {} });
+  } catch (e) {}
+  return Promise.resolve(true);
+};
+
+/**
+ * @description
+ * A storage service with IndexedDB.
+ */
+export class IndexedDBStorage extends Dexie implements StorageInterface {
+  project!: Table<Project, number | string>;
+  group!: Table<Group, number | string>;
+  environment!: Table<Environment, number | string>;
+  apiData!: Table<ApiData, number | string>;
+  apiTestHistory!: Table<ApiTestHistory, number | string>;
+  mock!: Table<ApiMockEntity, number | string>;
+
+  constructor() {
+    super('eoapi_core');
+    this.version(2).stores({
+      project: '++uuid, name',
+      environment: '++uuid, name, projectID',
+      group: '++uuid, name, projectID, parentID',
+      apiData: '++uuid, name, projectID, groupID',
+      apiTestHistory: '++uuid, projectID, apiDataID',
+      mock: '++uuid, name, apiDataID, projectID, createWay',
+    });
+    this.open();
+    this.on('populate', () => this.populate());
+  }
+
+  async populate() {
+    await this.project.add({ uuid: 1, name: 'Default' });
+    await this.apiData.bulkAdd(sampleApiData);
+  }
+
+  private resProxy(data): ResultType {
+    const result = {
+      status: StorageResStatus.success,
+      data,
+    };
+    return result as ResultType;
+  }
+
+  add(table: Table, items: Array<StorageItem>): object {
+    const time = Date.now();
+    const result = table.bulkAdd(
+      items.map((item: StorageItem) => ({
+        createdAt: time,
+        updatedAt: time,
+        ...item,
+      }))
+    );
+    return this.resProxy(result);
+  }
+
+  remove(table: Table, uuids: Array<number | string>): object {
+    const result = table.bulkDelete(uuids);
+    return this.resProxy(result);
+  }
+
+  load(table: Table, uuids: Array<number | string>): object {
+    return new Promise((resolve, reject) => {
+      table
+        .bulkGet(uuids)
+        .then((result) => {
+          resolve(this.resProxy(result));
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  update(table: Table, items: Array<StorageItem>): object {
+    const time = Date.now();
+    const list: any = items.map((item: StorageItem) => ({
+      ...item,
+      updatedAt: time,
+    }));
+    return new Promise((resolve, reject) => {
+      table
+        .bulkGet(list)
+        .then((res) => resolve(res))
+        .catch((err) => reject(err));
+    });
+  }
+}
