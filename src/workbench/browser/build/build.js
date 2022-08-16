4fc172abad527bc6437f16756a661da9e4e71705
scarqin <1054139596@qq.com>
2022-08-17 07:29:31 +0800

    web build gio

--- src/workbench/browser/build/build.js
+++ src/workbench/browser/build/build.js
@@ -32,6 +32,22 @@ class webPlatformBuilder {
          window.location.href=baseDir;
          }
         </script>
+        <!-- GrowingIO Analytics code version 2.1 -->
+<!-- Copyright 2015-2018 GrowingIO, Inc. More info available at http://www.growingio.com -->
+
+<script type='text/javascript'>
+  !function(e,t,n,g,i){e[i]=e[i]||function(){(e[i].q=e[i].q||[]).push(arguments)},n=t.createElement("script"),tag=t.getElementsByTagName("script")[0],n.defer=1,n.src=('https:'==document.location.protocol?'https://':'http://')+g,tag.parentNode.insertBefore(n,tag)}(window,document,"script","assets.giocdn.com/2.1/gio.js","gio");
+    gio('init','bd7645ddc19400a8', {});
+  
+  //custom page code begin here
+  
+  //custom page code end here
+  
+  gio('send');
+  
+  </script>
+  
+  <!-- End GrowingIO Analytics code version: 2.1 -->
       </head>
       <body></body>
     </html>
