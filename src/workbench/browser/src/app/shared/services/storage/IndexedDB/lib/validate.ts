acfcca12a9f35aece62823a47ccf6889cad534dd
scarqin <1054139596@qq.com>
2022-08-23 16:18:49 +0800

    feat: valid import apiData bodyType

--- src/workbench/browser/src/app/shared/services/storage/IndexedDB/lib/validate.ts
+++ src/workbench/browser/src/app/shared/services/storage/IndexedDB/lib/validate.ts
@@ -1,14 +1,28 @@
 import Ajv, { JSONSchemaType } from 'ajv';
 import { ApiData, Environment, Group } from '../../index.model';
+import { whatType } from 'eo/workbench/browser/src/app/utils';
 import apiDataSchema from '../schema/apiData.json';
+import { ApiBodyType } from '../../index.model';
 import envSchema from '../schema/env.json';
 export const parseAndCheckApiData = (apiData): { validate: boolean; data?: ApiData; error?: any } => {
   const ajv = new Ajv({
     useDefaults: true,
-    removeAdditional:true
+    removeAdditional: true,
   });
   const validate = ajv.compile<ApiData>(apiDataSchema);
   if (validate(apiData)) {
+    ['requestBody', 'responseBody'].forEach((keyName) => {
+      if (
+        [ApiBodyType['Form-data'], ApiBodyType.JSON, ApiBodyType.XML].includes(apiData[`${keyName}Type`]) &&
+        whatType(apiData[keyName] !== 'array')
+      ) {
+        //Handle xml\formdata\json  data
+        apiData[keyName] = [];
+      } else if (whatType(apiData[keyName] !== 'string')) {
+        //Handle raw\binary data
+        apiData[keyName] = '';
+      }
+    });
     return { validate: true, data: apiData };
   } else {
     console.error(validate.errors);
@@ -33,7 +47,7 @@ export const parseAndCheckGroup = (group): { validate: boolean; data?: Group } =
 export const parseAndCheckEnv = (env): { validate: boolean; data?: Environment; error?: any } => {
   const ajv = new Ajv({
     useDefaults: true,
-    removeAdditional:true
+    removeAdditional: true,
   });
   const validate = ajv.compile<Environment>(envSchema);
   if (validate(env)) {
