a2339f33c41a3205b23f5f723c398626bbc350cc
scarqin <1054139596@qq.com>
2022-09-01 17:29:38 +0800

    feat: graph-ql

---
+++ src/workbench/browser/src/app/pages/api/graph-ql/test/graph-ql-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eo-graph-ql-test',
+  templateUrl: './graph-ql-test.component.html',
+  styleUrls: ['./graph-ql-test.component.scss']
+})
+export class GraphQlTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
