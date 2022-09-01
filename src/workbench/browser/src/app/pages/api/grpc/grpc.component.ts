7df4730c1f53fcd57b05592a6325d8f8eccfc53d
buqiyuan <1743369777@qq.com>
2022-09-01 16:33:05 +0800

    grpc stop

--- src/workbench/browser/src/app/pages/api/grpc/grpc.component.ts
+++ src/workbench/browser/src/app/pages/api/grpc/grpc.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'eo-grpc',
@@ -6,5 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grpc.component.scss'],
 })
 export class GrpcComponent implements OnInit {
-  ngOnInit(): void {}
+  validateForm!: UntypedFormGroup;
+
+  submitForm(): void {
+    console.log('submit', this.validateForm.value);
+  }
+
+  constructor(private fb: UntypedFormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      url: [null, [Validators.required]],
+      serviceName: [null, [Validators.required]],
+      methodName: [null, [Validators.required]],
+    });
+  }
 }
