579b44d71da1a929674abd0cc65b85f0f9368f09
夜鹰 <17kungfuboy@gmail.com>
2022-09-09 15:47:31 +0800

    feat: local start socket server automatical

--- src/workbench/node/server/main.js
+++ src/workbench/node/server/main.js
@@ -9,11 +9,6 @@ const koaBody = require('koa-body');
 const Koa = require('koa');
 const cors = require('@koa/cors');
 
-const IO = require('socket.io');
-const WebSocket = require('ws');
-
-const io = new IO.Server(4301);
-
 const app = new Koa();
 const port = 4201;
 
@@ -50,84 +45,3 @@ app.use(async (ctx, next) => {
 });
 
 app.listen(port);
-
-io.on('connection', (socket) => {
-  // send a message to the client
-  socket.emit('ws-client', 'link success');
-  let ws = null;
-
-  // receive a message from the client
-  socket.on('ws-server', ({ type, content }) => {
-    if (type === 'connect') {
-      return;
-    }
-    if (type === 'ws-disconnect') {
-      ws = null;
-      return;
-    }
-    if (type === 'ws-connect') {
-      const { request } = content;
-      // console.log(request?.requestHeaders);
-      // try {
-      //   new WebSocket(request.uri);
-      // } catch (error) {
-      //   console.log('try to get the error', error);
-      // }
-      try {
-        ws = new WebSocket(request.protocol + request.uri, {
-          headers: request?.requestHeaders
-            ?.filter((it) => it.name && it.value)
-            .reduce(
-              (total, { name, value }) => ({
-                ...total,
-                [name]: value,
-              }),
-              {}
-            ),
-        });
-        ws.on('error', (err) => {
-          socket.emit('ws-client', { type: 'ws-connect-back', status: -1, content: err });
-        });
-      } catch (error) {
-        socket.emit('ws-client', { type: 'ws-connect-back', status: -1, content: error });
-        ws = null;
-        return;
-      }
-
-      const reqHeader = ws._req.getHeaders();
-      // console.log(ws);
-
-      // 打开WebSocket连接后立刻发送一条消息:
-      ws.on('open', () => {
-        // console.log(`[CLIENT] open()`);
-      });
-      ws.on('upgrade', (res) => {
-        const { headers: resHeader } = res;
-        socket.emit('ws-client', { type: 'ws-connect-back', status: 0, content: { reqHeader, resHeader } });
-      });
-
-      ws.on('message', (message) => {
-        socket.emit('ws-client', {
-          type: 'ws-message-back',
-          status: 0,
-          content: message,
-        });
-      });
-
-      ws.on('close', () => {
-        socket.emit('ws-client', {
-          type: 'ws-message-back',
-          status: 1,
-          content: 'Server disconnected',
-        });
-      });
-    }
-    if (type === 'ws-message') {
-      const { message } = content;
-      if (!message) {
-        console.log('发送内容为空');
-      }
-      ws.send(message);
-    }
-  });
-});
