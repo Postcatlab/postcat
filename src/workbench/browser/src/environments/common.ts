baa58defc44f6832f9a8007cefe3ee257dc1355f
夜鹰 <17kungfuboy@gmail.com>
2022-09-13 16:13:50 +0800

    feat: update config

--- src/workbench/browser/src/environments/common.ts
+++ src/workbench/browser/src/environments/common.ts
@@ -4,7 +4,7 @@ export type APP_CONFIG_INSTANT = {
   production: boolean;
   environment: 'DEV' | 'PROD';
   EXTENSION_URL: string;
-  REMOTE_SOCKET_URL: 'wss://8.219.85.124';
+  REMOTE_SOCKET_URL: 'wss://www.eoapi.io';
   SOCKET_PORT: number;
   MOCK_URL: string;
 };
