464417b41aadc71481356186f62d0f57ab1b2f0f
夜鹰 <17kungfuboy@gmail.com>
2022-09-13 19:11:34 +0800

    feat: change socket server url

--- src/workbench/browser/src/environments/common.ts
+++ src/workbench/browser/src/environments/common.ts
@@ -4,7 +4,7 @@ export type APP_CONFIG_INSTANT = {
   production: boolean;
   environment: 'DEV' | 'PROD';
   EXTENSION_URL: string;
-  REMOTE_SOCKET_URL: 'wss://www.eoapi.io';
+  REMOTE_SOCKET_URL: 'wss://eoapi.eolinker.com';
   SOCKET_PORT: number;
   MOCK_URL: string;
 };
