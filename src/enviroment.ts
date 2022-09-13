cada91c87294953f08c31cddfd7f5f3eeb85b6c6
夜鹰 <17kungfuboy@gmail.com>
2022-09-13 16:13:50 +0800

    feat: update config

--- src/enviroment.ts
+++ src/enviroment.ts
@@ -1,6 +1,6 @@
 export const ELETRON_APP_CONFIG = {
   EXTENSION_URL: 'http://106.12.149.147',
-  REMOTE_SOCKET_URL: 'wss://8.219.85.124',
+  REMOTE_SOCKET_URL: 'wss://www.eoapi.io',
   SOCKET_PORT: 4301,
   MOCK_URL: 'https://mockapi.eolink.com/ztBFKai20ee60c12871881565b5a6ddd718337df0e30979',
   NODE_SERVER_PORT: 4201,
