f8263a17f0ce8eaf4694c2e5e2f86df9ebff4ec3
scarqin <1054139596@qq.com>
2022-11-01 21:13:21 +0800

    fix: router error

--- src/workbench/browser/src/app/app-routing.module.ts
+++ src/workbench/browser/src/app/app-routing.module.ts
@@ -26,11 +26,6 @@ const routes: Routes = [
   exports: [RouterModule],
   // 👇 设置基础路由
   providers: [
-    {
-      provide: APP_BASE_HREF,
-      // @ts-ignore __MICRO_APP_BASE_ROUTE__ 为micro-app传入的基础路由
-      useValue: window.__MICRO_APP_BASE_ROUTE__ || '/',
-    },
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
