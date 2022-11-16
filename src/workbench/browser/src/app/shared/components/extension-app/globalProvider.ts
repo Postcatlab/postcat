58195afda8adb5728c114713f0d0cb8459a785d1
buqiyuan <1743369777@qq.com>
2022-11-16 20:20:08 +0800

    feat: add getExtensionSettings soppurt in web

--- src/workbench/browser/src/app/shared/components/extension-app/globalProvider.ts
+++ src/workbench/browser/src/app/shared/components/extension-app/globalProvider.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ModalService } from 'eo/workbench/browser/src/app/shared/services/modal.service';
 import { NavigationExtras, Router } from '@angular/router';
+import { SettingService } from 'eo/workbench/browser/src/app/core/services/settings/settings.service';
 
 @Injectable({ providedIn: 'root' })
 export class GlobalProvider {
-  constructor(private modalService: ModalService, private router: Router) {}
+  constructor(private modalService: ModalService, private router: Router, private settingService: SettingService) {}
 
   injectGlobalData() {
     window.eo ??= {};
     window.eo.modalService = this.modalService;
+    window.eo.getExtensionSettings = this.settingService.getConfiguration;
     window.eo.navigate = (commands: any[], extras?: NavigationExtras) => {
       this.router.navigate(commands, extras);
     };
