2ed57392957dc81db13dd96679b99432182a0b2a
kungfuboy <17kungfuboy@gmail.com>
2022-11-17 17:49:13 +0800

    delete log

--- src/workbench/browser/src/app/pages/member.module.ts
+++ src/workbench/browser/src/app/pages/member.module.ts
@@ -4,7 +4,6 @@ import { NzModalService, NzModalModule } from 'ng-zorro-antd/modal';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { FormsModule } from '@angular/forms';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { MessageService } from 'eo/workbench/browser/src/app/shared/services/message/message.service';
 import { ManageAccessComponent } from 'eo/workbench/browser/src/app/shared/components/manage-access/manage-access.component';
 import { SharedModule } from 'eo/workbench/browser/src/app/shared/shared.module';
 
