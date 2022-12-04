451b4268bc883c8afefaec71afed1b173f4836a8
scarqin <1054139596@qq.com>
2022-12-04 22:57:56 +0800

    feat: add monaco

--- src/workbench/browser/src/app/modules/eo-ui/monaco-editor/monaco.module.ts
+++ src/workbench/browser/src/app/modules/eo-ui/monaco-editor/monaco.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EoMonacoEditorComponent } from './monaco-editor.component';
-// import { NzCodeEditorModule } from 'ng-zorro-antd/code-editor';
+import { NzCodeEditorModule } from 'ng-zorro-antd/code-editor';
 import { FormsModule } from '@angular/forms';
 import { EoIconparkIconModule } from '../iconpark-icon/eo-iconpark-icon.module';
 
 @NgModule({
   declarations: [EoMonacoEditorComponent],
-  imports: [CommonModule,FormsModule,EoIconparkIconModule],
+  imports: [CommonModule,FormsModule,EoIconparkIconModule, NzCodeEditorModule],
   exports: [EoMonacoEditorComponent],
   schemas: [],
 })
