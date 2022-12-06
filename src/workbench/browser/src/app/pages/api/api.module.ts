32c6bf5286b5488a0d675fb3f0fcc0e79c451591
scarqin <1054139596@qq.com>
2022-12-06 22:43:10 +0800

    fix: group indent error

--- src/workbench/browser/src/app/pages/api/api.module.ts
+++ src/workbench/browser/src/app/pages/api/api.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 import { ApiRoutingModule } from './api-routing.module';
