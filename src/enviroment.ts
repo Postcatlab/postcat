378eac4ed54239541b81daec5386a7ba44059ec3
buqiyuan <1743369777@qq.com>
2022-12-07 15:41:27 +0800

    chore: update env file

--- src/enviroment.ts
+++ src/enviroment.ts
@@ -1,10 +1,10 @@
 export const ELETRON_APP_CONFIG = {
-  EXTENSION_URL: 'http://8.219.85.124:5000',
-  // EXTENSION_URL: 'http://106.12.149.147',
+  // EXTENSION_URL: 'http://8.219.85.124:5000',
+  EXTENSION_URL: 'http://106.12.149.147',
   REMOTE_SOCKET_URL: 'wss://eoapi.eolinker.com',
   // SOCKET_PORT: '',
-  // MOCK_URL: 'https://mockapi.eolink.com/ztBFKai20ee60c12871881565b5a6ddd718337df0e30979',
-  MOCK_URL: 'https://mockapi.eolink.com/CYVQc8d65112bb0db65ca66cfbbfdc3ffd4c5475fd26efe',
+  MOCK_URL: 'https://mockapi.eolink.com/ztBFKai20ee60c12871881565b5a6ddd718337df0e30979',
+  // MOCK_URL: 'https://mockapi.eolink.com/CYVQc8d65112bb0db65ca66cfbbfdc3ffd4c5475fd26efe',
   // MOCK_URL: 'http://8.219.85.124:5000',
   NODE_SERVER_PORT: 4201,
 };
