521f83ec00ea5ae8fe14588e3e346ad51665ce88
kungfuboy <17kungfuboy@gmail.com>
2022-12-11 21:38:55 +0800

    update

--- src/workbench/browser/src/app/modules/api-shared/api-shared.module.ts
+++ src/workbench/browser/src/app/modules/api-shared/api-shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { EoNgTabsModule } from 'eo-ng-tabs';
 import { EoNgTreeModule } from 'eo-ng-tree';
 import { ApiMockTableComponent } from 'eo/workbench/browser/src/app/modules/api-shared/api-mock-table.component';
+import { ParamsImportComponent } from 'eo/workbench/browser/src/app/modules/api-shared/params-import/params-import.component';
 import { EoMonacoEditorModule } from 'eo/workbench/browser/src/app/modules/eo-ui/monaco-editor/monaco.module';
 import { ApiMockService } from 'eo/workbench/browser/src/app/pages/api/http/mock/api-mock.service';
 import { NzEmptyModule } from 'ng-zorro-antd/empty';
@@ -10,20 +11,18 @@ import { ApiTestService } from '../../pages/api/http/test/api-test.service';
 import { SharedModule } from '../../shared/shared.module';
 import { EoTableProModule } from '../eo-ui/table-pro/table-pro.module';
 import { ApiParamsNumPipe } from './api-param-num.pipe';
+import { ApiTableService } from './api-table.service';
 import { ApiTestHeaderComponent } from './api-test-header/api-test-header.component';
 import { ApiTestQueryComponent } from './api-test-query/api-test-query.component';
 import { ApiTestResultHeaderComponent } from './api-test-result-header/api-test-result-header.component';
-
 import { ApiTestUtilService } from './api-test-util.service';
-import { ParamsImportComponent } from './params-import/params-import.component';
-import { ApiTableService } from './api-table.service';
 
 const COMPONENTS = [
   ApiTestHeaderComponent,
   ParamsImportComponent,
   ApiTestQueryComponent,
   ApiTestResultHeaderComponent,
-  ApiMockTableComponent,
+  ApiMockTableComponent
 ];
 const SHARE_UI = [EoTableProModule, EoNgTabsModule];
 
@@ -31,6 +30,6 @@ const SHARE_UI = [EoTableProModule, EoNgTabsModule];
   imports: [SharedModule, EoMonacoEditorModule, EoNgTreeModule, NzEmptyModule, ...SHARE_UI],
   declarations: [...COMPONENTS, ApiParamsNumPipe],
   providers: [ApiTestUtilService, ApiTableService, ApiTestService, ApiMockService],
-  exports: [...COMPONENTS, ApiParamsNumPipe, ...SHARE_UI],
+  exports: [...COMPONENTS, ApiParamsNumPipe, ...SHARE_UI]
 })
 export class ApiSharedModule {}
--- src/workbench/browser/src/app/modules/api-shared/params-import/params-import.component.ts
+++ src/workbench/browser/src/app/modules/api-shared/params-import/params-import.component.ts
@@ -1,15 +1,30 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { EoNgFeedbackMessageService } from 'eo-ng-feedback';
+import { cloneDeep } from 'lodash-es';
+import { computed, observable, makeObservable, reaction } from 'mobx';
 import qs from 'qs';
 
 import { form2json, xml2json, isXML, json2Table } from '../../../utils/data-transfer/data-transfer.utils';
-import { eoDeepCopy, whatType } from '../../../utils/index.utils';
+import { whatType } from '../../../utils/index.utils';
+
+const titleHash = new Map()
+  .set('xml', 'XML')
+  .set('json', 'JSON')
+  .set('formData', 'Form-data')
+  .set('header', $localize`Header`);
+
+const egHash = new Map()
+  .set('xml', '<name>Jack</name>')
+  .set('formData', 'name: Jack\nage: 18')
+  .set('query', '/api?name=Jack&age=18')
+  .set('json', `{ "name": "Jack", "age": 18 }`)
+  .set('header', 'headerName:headerValue\nheaderName2:headerValue2');
 @Component({
   selector: 'params-import',
   templateUrl: './params-import.component.html',
   styleUrls: ['./params-import.component.scss']
 })
-export class ParamsImportComponent {
+export class ParamsImportComponent implements OnInit {
   @Input() disabled: boolean;
   @Input() rootType: 'array' | string | 'object' = 'object';
   @Input() contentType: string | 'json' | 'formData' | 'xml' | 'header' | 'query' = 'json';
@@ -17,148 +32,114 @@ export class ParamsImportComponent {
   @Input() modalTitle = '';
   @Output() readonly baseDataChange = new EventEmitter<any>();
   @Output() readonly beforeHandleImport = new EventEmitter<any>();
-  isVisible = false;
+
+  @observable isVisible = false;
   paramCode = '';
+
+  @computed get contentTypeTitle() {
+    return titleHash.get(this.contentTypeTitle) || '';
+  }
+
+  @computed get eg() {
+    return egHash.get(this.contentTypeTitle);
+  }
+
+  @computed get contentTypeEditor() {
+    return ['formData', 'header', 'json'].includes(this.contentType) ? 'text' : this.contentType;
+  }
+
   constructor(private message: EoNgFeedbackMessageService) {}
 
-  get contentTypeTitle() {
-    switch (this.contentType) {
-      case 'xml':
-        return 'XML';
-      case 'json':
-        return 'JSON';
-      case 'formData':
-        return `Form-data`;
-      case 'header':
-        return $localize`Header`;
-      default:
-        return '';
-    }
+  ngOnInit() {
+    makeObservable(this);
+    reaction(
+      () => this.isVisible,
+      () => {
+        this.paramCode = '';
+      }
+    );
   }
-
-  get eg() {
-    switch (this.contentType) {
-      case 'xml':
-        return `<name>Jack</name>`;
-      // case 'json':
-      //   return `{ "name": "Jack", "age": 12 }`;
-      case 'formData':
-        return `name: Jack\nage: 18`;
-      case 'query':
-        return `/api?name=Jack&age=18`;
-      case 'json':
-        return `{ "name": "Jack", "age": 18}`;
-      case 'header':
-        return `headerName:headerValue\nheaderName2:headerValue2`;
-      default:
-        return `/api?name=Jack&age=18`;
-    }
-  }
-
-  get contenTypeEditor() {
-    switch (this.contentType) {
-      case 'formData':
-      case 'header':
-      case 'query':
-        return 'text';
-      default:
-        return this.contentType;
-    }
-  }
-
   showModal(type): void {
     this.isVisible = true;
   }
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+
+  parseJSON(code) {
+    // * parse json
+    let data;
+    try {
+      data = JSON.parse(code);
+    } catch (error) {
+      return [null, { msg: $localize`JSON format invalid` }];
+    }
+    return [{ data, rootType: Array.isArray(data) ? 'array' : 'object' }, null];
+  }
+
+  parseQuery(code) {
+    const data = qs.parse(code.indexOf('?') > -1 ? code.split('?')[1] : code);
+    return [{ data, rootType: 'object' }, null];
+  }
+
+  parseXML(code) {
+    const status = isXML(code);
+    if (!status) {
+      return [null, { msg: $localize`XML format invalid` }];
+    }
+    return [{ data: xml2json(code), rootType: 'object' }, null];
+  }
+
+  parseRaw(code) {
+    return [{ data: code, rootType: null }, null];
+  }
+
+  parseForm(code) {
+    const data = form2json(code).reduce((total, it) => ({ ...total, ...it }), {});
+    return [{ data: data, rootType: 'object' }, null];
+  }
 
   handleImport(type: string): void {
-    if (this.paramCode === '') {
+    if (this.paramCode.trim() === '') {
       this.handleCancel();
       return;
     }
-    let rootType = this.rootType;
-    let paramCode = null;
-    if (this.contentType === 'json') {
-      try {
-        paramCode = JSON.parse(this.paramCode);
-        this.beforeHandleImport.emit(paramCode);
-        rootType = Array.isArray(paramCode) ? 'array' : 'object';
-      } catch (error) {
-        this.message.error($localize`JSON format invalid`);
-        return;
-      }
-    }
-    if (this.contentType === 'query') {
-      paramCode = qs.parse(this.paramCode.indexOf('?') > -1 ? this.paramCode.split('?')[1] : this.paramCode);
-    }
-    if (['formData', 'header'].includes(this.contentType)) {
-      const json = {};
-      form2json(this.paramCode).forEach(it => {
-        const { key, value } = it;
-        if (key == null || value == null) {
-          this.message.error($localize`Form format invalid`);
-          return;
-        }
-        json[key] = value;
-      });
-      paramCode = json;
-    }
-    if (this.contentType === 'xml') {
-      const status = isXML(this.paramCode);
-      if (!status) {
-        this.message.error($localize`XML format invalid`);
-        return;
-      }
-      paramCode = xml2json(this.paramCode);
-    }
-    if (this.contentType === 'raw') {
-      paramCode = this.paramCode;
-    }
-
+    const codeType = whatType(this.paramCode);
     const tailData = this.baseData.slice(-1);
-    let resultData = eoDeepCopy(this.baseData.reverse().slice(1).reverse());
-    // console.log('resultData', resultData);
-
-    if (rootType !== whatType(paramCode)) {
+    const resultData = cloneDeep(this.baseData.reverse().slice(1).reverse());
+    if (this.rootType !== codeType) {
       // TODO Perhaps should be handled about format compatibility later.
       console.warn('EO_WARN[params-import]: The code that you input is no-equal to the root type.');
     }
-    // if (whatType(paramCode) === 'object') {
-    // * transform to array of table format.
-    // }
-    if (rootType === 'array' && whatType(paramCode) === 'array') {
-      // * only select first data
-      const [data] = paramCode;
-      paramCode = data || {};
-    }
-    // * tree to array for table render
-    const cacheData = json2Table(paramCode);
+    const func = {
+      json: this.parseJSON,
+      query: this.parseQuery,
+      xml: this.parseXML,
+      raw: this.parseRaw,
+      header: this.parseForm,
+      formData: this.parseForm
+    };
 
-    // TODO delete useless attribute in cacheData
-    switch (type) {
-      case 'mixin': {
-        const nameList = resultData.map(it => it.name);
-        const data = cacheData.filter(it => !nameList.includes(it.name));
-        resultData = resultData.concat(data);
-        break;
-      }
-      case 'overwrite': {
-        resultData = cacheData;
-        break;
-      }
-      case 'append': {
-        resultData = resultData.concat(cacheData);
-        break;
-      }
-      default:
-        break;
+    const [res, err] = func[this.contentType](this.paramCode);
+    if (err) {
+      this.message.error(err.msg);
+      return;
     }
-    console.log('tailData', resultData, tailData);
-    this.baseDataChange.emit(isXML(this.paramCode) ? resultData : resultData.concat(tailData));
-    this.handleCancel();
-  }
+    // * same data struct
+    if (codeType === 'array') {
+      console.log('===>', res.at(0));
+    }
+    const cacheData = json2Table(res);
 
-  handleCancel(): void {
-    this.paramCode = '';
-    this.isVisible = false;
+    const combineFunc = {
+      overwrite: data => data,
+      append: data => data.concat(cacheData),
+      mixin: data => {
+        const nameList = data.map(it => it.name);
+        return data.concat(cacheData.filter(it => !nameList.includes(it.name)));
+      }
+    };
+    const result = combineFunc[type](resultData);
+    this.baseDataChange.emit(isXML(this.paramCode) ? result : result.concat(tailData));
   }
 }
