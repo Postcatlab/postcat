78757bd5f7f82d45ae718747ed93a92172c4f18d
scarqin <1054139596@qq.com>
2022-12-13 15:06:13 +0800

    chore: merge

--- src/workbench/browser/src/app/utils/index.utils.ts
+++ src/workbench/browser/src/app/utils/index.utils.ts
@@ -169,24 +169,24 @@ export const copyText = async (text: string) => {
 };
 // fn 是需要防抖处理的函数
 // wait 是时间间隔
-// export function debounce(fn, wait = 50) {
-//   // 通过闭包缓存一个定时器 id
-//   let timer = null;
-//   // 将 debounce 处理结果当作函数返回
-//   // 触发事件回调时执行这个返回函数
-//   return function (...args) {
-//     // this保存给context
-//     // 如果已经设定过定时器就清空上一次的定时器
-//     if (timer) {
-//       clearTimeout(timer);
-//     }
+export function debounce(fn, wait = 50) {
+  // 通过闭包缓存一个定时器 id
+  let timer = null;
+  // 将 debounce 处理结果当作函数返回
+  // 触发事件回调时执行这个返回函数
+  return function (...args) {
+    // this保存给context
+    // 如果已经设定过定时器就清空上一次的定时器
+    if (timer) {
+      clearTimeout(timer);
+    }
 
-//     // 开始设定一个新的定时器，定时器结束后执行传入的函数 fn
-//     timer = setTimeout(() => {
-//       fn.apply(this, args);
-//     }, wait);
-//   };
-// }
+    // 开始设定一个新的定时器，定时器结束后执行传入的函数 fn
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, wait);
+  };
+}
 
 export function throttle(fn, gap) {
   let timerId = null;
