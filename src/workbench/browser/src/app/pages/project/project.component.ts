2b452dcf5c5f954e1edf88c2266c0d54d5bde882
buqiyuan <1743369777@qq.com>
2022-12-14 14:42:27 +0800

    feat: add project page

---
+++ src/workbench/browser/src/app/pages/project/project.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'project-management',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.scss']
+})
+export class ProjectComponent {
+  constructor() {}
+}
