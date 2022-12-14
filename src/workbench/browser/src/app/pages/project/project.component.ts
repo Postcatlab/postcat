4e0878b42106ef0e0c41bed562506a4df44a3de7
buqiyuan <1743369777@qq.com>
2022-12-14 18:22:09 +0800

    chore: remove unuse code

--- src/workbench/browser/src/app/pages/project/project.component.ts
+++ src/workbench/browser/src/app/pages/project/project.component.ts
@@ -1,11 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { EoNgFeedbackMessageService } from 'eo-ng-feedback';
 import { StorageRes, StorageResStatus } from 'eo/workbench/browser/src/app/shared/services/storage/index.model';
 import { StorageService } from 'eo/workbench/browser/src/app/shared/services/storage/storage.service';
 import { StoreService } from 'eo/workbench/browser/src/app/shared/store/state.service';
-
-const fakeDataUrl = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
 
 type ListType = 'list' | 'card';
 
@@ -18,20 +15,9 @@ export class ProjectComponent implements OnInit {
   listType: ListType = 'list';
   initLoading = true; // bug
   loadingMore = false;
-  data: any[] = [];
   projectList: any[] = [];
 
-  actionMenus = [
-    { title: '修改', click: this.editProject.bind(this) },
-    { title: '删除', click: this.delProject.bind(this) }
-  ];
-
-  constructor(
-    private http: HttpClient,
-    private msg: EoNgFeedbackMessageService,
-    private storage: StorageService,
-    private store: StoreService
-  ) {}
+  constructor(private msg: EoNgFeedbackMessageService, private storage: StorageService, private store: StoreService) {}
 
   ngOnInit(): void {
     this.getProjectList();
@@ -44,11 +30,6 @@ export class ProjectComponent implements OnInit {
         this.initLoading = false;
       }
     });
-  }
-
-  async getData(callback: (res: any) => void) {
-    const data = await fetch(fakeDataUrl).then(res => res.json());
-    callback(data);
   }
 
   editProject(item: any): void {
