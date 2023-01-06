8669906c73e33bb079739b24373e33e3a3f35458
buqiyuan <1743369777@qq.com>
2023-01-06 11:41:29 +0800

    chore: update config

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,24 +1,13 @@
-import { sign, doSign } from 'app-builder-lib/out/codeSign/windowsCodeSign';
+import { sign, doSign, CustomWindowsSign } from 'app-builder-lib/out/codeSign/windowsCodeSign';
 import { build, Platform } from 'electron-builder';
 import type { Configuration, BuildResult } from 'electron-builder';
-import minimist from 'minimist';
 
-import { exec, spawn } from 'node:child_process';
+import { exec } from 'node:child_process';
 import { copyFileSync } from 'node:fs';
 import path from 'node:path';
 import { exit, platform } from 'node:process';
 
-// 当前 postcat 版本
-const version = process.env.npm_package_version;
-// 保存签名时的参数，供签名后面生成的 自定义安装界面 安装包
-let signOptions: Parameters<typeof sign>;
-// 参数同 electron-builder cli 命令行参数
-const argv = minimist(process.argv.slice(2));
-// https://nodejs.org/docs/latest/api/util.html#util_class_util_textdecoder
-const decoder = new TextDecoder('gbk');
-
-// 删除 minimist 解析后默认带的 _ 属性，防止 electron-builder 执行报错
-Reflect.deleteProperty(argv, '_');
+let signOptions: Parameters<CustomWindowsSign>;
 
 // mac 系统删除 release 目录
 if (process.platform === 'darwin') {
@@ -76,12 +65,12 @@ const config: Configuration = {
     signingHashAlgorithms: ['sha256'],
     signDlls: false,
     certificateSubjectName: 'OID.1.3.6.1.4.1.311.60.2.1.3=CN, OID.2.5.4.15=Private Organization',
-    target: ['nsis']
-    // sign(configuration, packager) {
-    //   // console.log('configuration', configuration);
-    //   signOptions = [configuration, packager!];
-    //   return doSign(configuration, packager!);
-    // }
+    target: ['nsis'],
+    sign(configuration, packager) {
+      // console.log('configuration', configuration);
+      signOptions = [configuration, packager];
+      return doSign(configuration, packager!);
+    }
   },
   portable: {
     splashImage: 'src/app/common/images/postcat.bmp'
@@ -104,63 +93,44 @@ const config: Configuration = {
     target: ['AppImage']
   }
 };
-
-// 要打包的目标平台
 const targetPlatform: Platform = {
   darwin: Platform.MAC,
   win32: Platform.WINDOWS,
   linux: Platform.LINUX
 }[platform];
 
-// 针对 Windows 签名
-const signWindows = async () => {
-  if (process.platform !== 'win32') return;
-
-  // 给卸载程序签名
-  signOptions[0] = {
-    ...signOptions[0],
-    path: 'D:\\git\\postcat\\build\\Uninstall Postcat.exe'
-  };
-  await sign(...signOptions);
-
-  copyFileSync(
-    path.join(__dirname, '../build', 'Uninstall Postcat.exe'),
-    path.join(__dirname, '../release/win-unpacked', 'Uninstall Postcat.exe')
-  );
-  // 生成 自定义安装包
-  exec(`yarn wininstaller`);
-
-  const ls = spawn('yarn', ['wininstaller'], {
-    // 仅在当前运行环境为 Windows 时，才使用 shell
-    shell: process.platform === 'win32'
-  });
-
-  ls.stdout.on('data', async data => {
-    if (decoder.decode(data).includes('请按任意键继续')) {
-      // 给自定义安装包签名
-      signOptions[0] = {
-        ...signOptions[0],
-        path: `D:\\git\\postcat\\release\\Postcat-Setup-${version}.exe`
-      };
-      await sign(...signOptions);
-
-      console.log('\x1b[32m', '打包完成🎉🎉🎉你要的都在 release 目录里🤪🤪🤪');
-      exit();
-    }
-  });
-};
-
-console.log('打包参数', argv);
-
 Promise.all([
   build({
     config,
-    targets: targetPlatform.createTarget(),
-    ...argv
+    targets: targetPlatform.createTarget()
   })
 ])
-  .then(() => {
-    // signWindows();
+  .then(async () => {
+    console.log('\x1b[32m', '打包完成🎉🎉🎉你要的都在 release 目录里🤪🤪🤪');
+
+    signOptions[0] = {
+      ...signOptions[0],
+      path: 'D:\\git\\postcat\\build\\Uninstall Postcat.exe'
+    };
+    // @ts-ignore
+    await sign(...signOptions);
+
+    copyFileSync(
+      path.join(__dirname, '../build', 'Uninstall Postcat.exe'),
+      path.join(__dirname, '../release/win-unpacked', 'Uninstall Postcat.exe')
+    );
+
+    exec(`yarn wininstaller`);
+
+    setTimeout(async () => {
+      signOptions[0] = {
+        ...signOptions[0],
+        path: 'D:\\git\\postcat\\release\\Postcat-Setup-0.0.1-beta.exe'
+      };
+      // @ts-ignore
+      await sign(...signOptions);
+      exit();
+    }, 60000);
   })
   .catch(error => {
     console.log('\x1b[31m', '打包失败，错误信息：', error);
