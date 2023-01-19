e09df39a44ff4b415d29ccec18fc41806d98561d
scarqin <1054139596@qq.com>
2023-01-19 10:11:26 +0800

    perf(env-select): move env select

--- src/workbench/browser/src/app/modules/system-setting/common/data-storage.component.ts
+++ src/workbench/browser/src/app/modules/system-setting/common/data-storage.component.ts
@@ -15,7 +15,7 @@ import { SettingService } from '../settings.service';
         <div class="text-[12px] mt-[8px] text-tips">
           <p>
             <span i18n> Cloud Storage: Store data on the cloud for team collaboration and product use across devices.</span>
-            <a i18n href="https://docs.postcat.com/docs/storage.html" target="_blank" class="eo-link"> Learn more..</a>
+            <a i18n href="https://docs.postcat.com/docs/storage.html" target="_blank"> Learn more..</a>
           </p>
         </div>
       </nz-form-item>
