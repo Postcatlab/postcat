870fa88979424face4ce06cc47b120f37c057873
buqiyuan <1743369777@qq.com>
2023-01-19 10:10:36 +0800

    feat: update apiHistoryBulkDelete

--- src/workbench/browser/src/app/shared/services/storage/db/services/apiTestHistory.service.ts
+++ src/workbench/browser/src/app/shared/services/storage/db/services/apiTestHistory.service.ts
@@ -3,7 +3,17 @@ import { ApiTestHistory } from 'eo/workbench/browser/src/app/shared/services/sto
 import { BaseService } from 'eo/workbench/browser/src/app/shared/services/storage/db/services/base.service';
 
 export class ApiTestHistoryService extends BaseService<ApiTestHistory> {
+  baseService = new BaseService(dataSource.apiTestHistory);
+
   constructor() {
     super(dataSource.apiTestHistory);
+  }
+
+  async bulkDelete(params) {
+    const { ids, ...restParams } = params;
+    return this.baseService.bulkDelete({
+      id: ids,
+      ...restParams
+    });
   }
 }
