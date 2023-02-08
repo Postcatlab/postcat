78dabb3f73b88f6dde73a086d397a5b2d1a941fa
夜鹰 <17kungfuboy@gmail.com>
2023-02-08 15:29:57 +0800

    style: update

--- src/workbench/browser/src/app/shared/store/state.service.ts
+++ src/workbench/browser/src/app/shared/store/state.service.ts
@@ -92,12 +92,12 @@ export class StoreService {
   }
 
   @computed get isZh() {
-    return this.url.includes('/zh/');
+    return this.url.includes('/zh/home/');
   }
 
   @computed get isEn() {
-    // return this.url.includes('/en/');
-    return !this.url.includes('/zh/');
+    // return this.url.includes('/en/home/');
+    return !this.url.includes('/zh/home/');
   }
 
   // ? data source
