ee6a4526b07b29c8906451bfd95c7706010038a5
buqiyuan <1743369777@qq.com>
2023-02-13 14:36:48 +0800

    chore: 0.3.0

--- scripts/afterBuild.js
+++ scripts/afterBuild.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const glob = require('glob');
 const { resolve } = require('path');
 
-glob.sync(resolve('./release', './*.exe*')).forEach((item, i) => {
+glob.sync(resolve('./release', './*.exe.*')).forEach((item, i) => {
   const newName = item.replace(/ /g, '-');
   fs.rename(item, newName, err => {
     if (err) throw err;
