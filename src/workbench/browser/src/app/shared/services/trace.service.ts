b972d4f9afaf567832cb6ae89f8ec3c2ad17ae73
scarqin <1054139596@qq.com>
2023-02-15 23:26:31 +0800

    fix: gio bind userID

--- src/workbench/browser/src/app/shared/services/trace.service.ts
+++ src/workbench/browser/src/app/shared/services/trace.service.ts
@@ -15,6 +15,9 @@ export class TraceService {
     // console.log('trace =>>', eventId, JSON.stringify(params, null, 2));
     gio('track', eventId, params);
   }
+  setUserID(id) {
+    gio('setUserId', id);
+  }
   setUser(data = {}) {
     gio('people.set', data);
   }
