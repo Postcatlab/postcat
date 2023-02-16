438aaad9801d96589ffacff0a832dddc8d9c54f3
bqy_fe <1743369777@qq.com>
2023-02-16 18:22:26 +0800

    fix: mock match bug

--- src/workbench/browser/src/app/shared/services/mock.service.ts
+++ src/workbench/browser/src/app/shared/services/mock.service.ts
@@ -27,6 +27,7 @@ export class MockService {
         const sender = event.sender;
         // this.isRemoteMock = mockReg.test(req.url);
         const { mockID } = req.query;
+        const replyMsg = data => sender.send('getMockApiList', data);
 
         // if (isRemoteMock) {
         //   const [_, projectID] = req.url.match(mockReg);
@@ -55,30 +56,30 @@ export class MockService {
           try {
             const mock = await this.getMockByMockID(Number(mockID));
             if (mock === null) {
-              return {
+              return replyMsg({
                 statusCode: 404,
                 response: {
                   message: `mockID为${mockID}的mock不存在`
                 }
-              };
+              });
             }
             const apiData = await this.getApiData(String(mock.apiUuid));
             if (apiData === null) {
-              return { statusCode: 404 };
+              return replyMsg({ statusCode: 404 });
             }
             if (mock?.createWay === 'system') {
               // console.log('apiData.responseBody', apiData.responseBody);
-              return sender.send('getMockApiList', this.matchApiData(apiData, req));
+              return replyMsg(await this.matchApiData(apiData, req));
             } else {
               const result = await this.matchApiData(apiData, req);
               if (result.statusCode === 404) {
-                return sender.send('getMockApiList', result);
+                return replyMsg(result);
               }
               mock.response ??= this.generateResponse(apiData.responseList?.[0]?.responseParams.bodyParams);
             }
-            sender.send('getMockApiList', mock);
+            replyMsg(mock);
           } catch (error) {
-            sender.send('getMockApiList', {
+            replyMsg({
               response: {
                 message: error
               }
@@ -87,7 +88,7 @@ export class MockService {
           // Whether the matching request mode is enabled
         } else {
           const response = await this.batchMatchApiData(1, req);
-          sender.send('getMockApiList', response);
+          replyMsg(response);
         }
       });
     }
@@ -109,35 +110,38 @@ export class MockService {
    * @param req
    * @returns
    */
-  matchApiData(apiData: ApiData, req?) {
-    const { requestParams, responseList, apiAttrInfo } = apiData;
-    const { restParams, queryParams } = requestParams || {};
-    const { requestMethod } = apiAttrInfo || {};
-    const bodyParams = responseList?.[0]?.responseParams?.bodyParams;
+  async matchApiData(apiData: ApiData, req?) {
+    const { requestMethod, apiUuid } = apiData;
     const { pathname } = new URL(req.url, 'http://localhost:3040');
-    let uri = apiData.uri.trim();
+
+    let uri = apiData.uri.trim().replace(/\{(.+?)\}/g, (match, p) => '[^/]+');
     let isQueryMatch = true;
-    // if (Array.isArray(restParams) && restParams.length > 0) {
-    //   const restMap = restParams.reduce((p, c) => ((p[c.name] = c.paramAttr.example), p), {});
-    //   uri = uri.replace(/\{(.+?)\}/g, (match, p) => restMap[p] ?? match);
-    //   console.log('restMap', restMap);
+
+    // if (Array.isArray(queryParams) && queryParams.length > 0) {
+    //   const query = req.query;
+    //   isQueryMatch = queryParams.filter(n => n.isRequired).every(n => Reflect.has(query, n.name));
     // }
-    if (Array.isArray(queryParams) && queryParams.length > 0) {
-      const query = req.query;
-      isQueryMatch = queryParams.every(n => query[n.name]);
-    }
     const uriReg = new RegExp(`^/?${uri}/?$`);
     // @ts-ignore
     const isMatch = requestMethod === RequestMethod[req.method] && uriReg.test(decodeURIComponent(pathname)) && isQueryMatch;
-    return isMatch ? { response: this.generateResponse(bodyParams) } : { statusCode: 404 };
+
+    if (isMatch) {
+      const [apiDatas] = await this.apiServiece.api_apiDataDetail({ withParams: 1, apiUuids: [apiUuid] });
+      const { responseList } = apiDatas.at(0);
+
+      const bodyParams = responseList?.[0]?.responseParams?.bodyParams;
+
+      return { response: this.generateResponse(bodyParams) };
+    } else {
+      return { statusCode: 404 };
+    }
   }
 
   async batchMatchApiData(projectID = 1, req) {
-    const [apiDatas] = await this.apiServiece.api_apiDataList({});
-    const [apiList] = await this.apiServiece.api_apiDataDetail({ withParams: 1, apiUuids: apiDatas.items.map(n => n.apiUuid) });
+    const [data] = await this.apiServiece.api_apiDataList({ pageSize: 200, statuses: [0] });
     let result;
-    for (const api of apiList) {
-      result = this.matchApiData(api, req);
+    for (const api of data.items) {
+      result = await this.matchApiData(api, req);
       if (result?.statusCode !== 404) {
         return result;
       }
@@ -162,8 +166,10 @@ export class MockService {
    * @returns
    */
   async getApiData(apiUuid: string, isRemoteMock = false): Promise<ApiData> {
-    const [apiList] = await this.apiServiece.api_apiDataDetail({ apiUuids: [apiUuid] });
-    return apiList?.[0];
+    const [apiList] = await this.apiServiece.api_apiDataDetail({ withParams: 1, apiUuids: [apiUuid] });
+    const apiData = apiList?.[0];
+    apiData.requestMethod = apiData.apiAttrInfo?.requestMethod;
+    return apiData;
     // return new Promise((resolve, reject) => {
     //   this.indexedDBStorage.apiDataLoad().subscribe(
     //     (res: any) => {
