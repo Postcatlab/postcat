7441c4d18e676917e9e0c4c76171701561be78d3
scarqin <1054139596@qq.com>
2023-02-21 11:11:49 +0800

    wip: try to remove nps tips

--- src/workbench/browser/src/app/app.component.ts
+++ src/workbench/browser/src/app/app.component.ts
@@ -11,5 +11,35 @@ export class AppComponent {
     window.tooglePcConsole = (isOpen = true) => {
       this.openConsole = isOpen;
     };
+
+    // this.removeNpsTips();
+  }
+  /**
+   * Remove NPS logo tips
+   */
+  removeNpsTips() {
+    const targetNode = document.body;
+    const config = { childList: true };
+
+    const callback = (mutationList, observer) => {
+      for (const mutation of mutationList) {
+        if (mutation.type === 'childList' && mutation.addedNodes.length) {
+          const npsDom = mutation.addedNodes[0];
+          if (!npsDom.id.includes('howxmSDK')) return;
+          setTimeout(() => {
+            const iframe = document.querySelectorAll('iframe');
+            iframe.forEach((dom: HTMLIFrameElement) => {
+              if (!dom.src.includes('howxm')) return;
+              const tipsDom = dom.contentWindow.document;
+              console.log(dom.contentWindow, tipsDom, 'A child node has been added or removed.');
+            });
+            observer.disconnect();
+          }, 2000);
+        }
+      }
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
   }
 }
