a3046ffbc1dc7c772b3527d64547fcd1530f3594
bqy_fe <1743369777@qq.com>
2023-02-23 14:58:22 +0800

    chore: update apis

--- src/workbench/browser/src/app/shared/services/storage/remote.service.ts
+++ src/workbench/browser/src/app/shared/services/storage/remote.service.ts
@@ -2135,7 +2135,7 @@ export class RemoteService {
 
     return new Promise<[T, null] | [null, any]>(resolve => {
       this.http
-        .put(`${prefix}/api/sync/batch-update`, {
+        .post(`${prefix}/api/sync/batch-update`, {
           projectUuid,
           workSpaceUuid,
           ...items
