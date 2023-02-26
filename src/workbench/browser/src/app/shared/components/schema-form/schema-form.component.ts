88bfd30bb350cc462551532add9c77a6a7888c15
buqiyuan <1743369777@qq.com>
2023-02-26 18:11:28 +0800

    fix: sync-api not reactive

--- src/workbench/browser/src/app/shared/components/schema-form/schema-form.component.ts
+++ src/workbench/browser/src/app/shared/components/schema-form/schema-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, UntypedFormControl, Validators } from '@angular/forms';
+import { debounce } from 'lodash-es';
 
 const compMap = {
   string: 'input',
@@ -10,7 +11,7 @@ const compMap = {
 @Component({
   selector: 'eo-schema-form',
   template: `
-    <form nz-form [formGroup]="validateForm" [nzNoColon]="true" class="form">
+    <form *ngIf="isInit && validateForm" nz-form [formGroup]="validateForm" [nzNoColon]="true" class="form">
       <nz-form-item nz-col class="flex-1" *ngFor="let field of objectKeys(properties)">
         <ng-container *ngIf="properties[field]?.label">
           <nz-form-label
@@ -73,9 +74,9 @@ const compMap = {
               [nzDisabled]="properties[field]?.disabled"
               formControlName="{{ field }}"
             >
-              <label *ngFor="let item of properties[field]?.oneOf" eo-ng-radio [nzValue]="item.default || item.const">{{
-                item.title
-              }}</label>
+              <label *ngFor="let item of properties[field]?.oneOf" eo-ng-radio [nzValue]="item.default ?? item.const">
+                {{ item.title }}
+              </label>
             </eo-ng-radio-group>
           </ng-container>
         </nz-form-control>
@@ -83,27 +84,46 @@ const compMap = {
     </form>
   `
 })
-export class EoSchemaFormComponent implements OnInit {
+export class EoSchemaFormComponent implements OnChanges {
   @Input() configuration = {} as any;
   @Input() model = {} as Record<string, any>;
+  @Output() readonly valueChanges: EventEmitter<any> = new EventEmitter<any>();
   validateForm!: FormGroup;
   objectKeys = Object.keys;
   properties = {};
   compMap = compMap;
+  isInit = true;
 
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit(): void {
-    this.init();
+  constructor(private fb: FormBuilder) {
+    this.validateForm = this.fb.group({});
+    this.initEmitter();
   }
 
-  init() {
-    this.validateForm = this.fb.group({});
+  ngOnChanges(changes: SimpleChanges) {
+    const modelIsNotEqual = !Object.is(changes.model?.previousValue, changes.model?.currentValue);
+    const configurationIsNotEqual = !Object.is(changes.configuration?.previousValue, changes.configuration?.currentValue);
+    if (modelIsNotEqual || configurationIsNotEqual) {
+      this.init();
+    }
+  }
 
+  init = debounce(() => {
+    this.isInit = false;
     this.formatProperties();
     this.initIfThenElse(this.configuration);
 
-    this.setSettingsModel(this.properties);
+    setTimeout(() => {
+      this.isInit = true;
+      this.setSettingsModel(this.properties);
+    });
+  }, 50);
+
+  initEmitter() {
+    this.validateForm.valueChanges.subscribe(
+      debounce(val => {
+        this.valueChanges.emit(val);
+      })
+    );
   }
 
   updateControls() {
@@ -141,7 +161,8 @@ export class EoSchemaFormComponent implements OnInit {
       configuration.allOf.forEach(item => {
         if (item.if?.properties) {
           Object.entries<any>(item.if.properties).forEach(([key, value]) => {
-            let __value;
+            let isInit = true;
+            let __value = this.model[key];
             Object.defineProperty(this.model, key, {
               configurable: true,
               get: () => {
@@ -149,9 +170,10 @@ export class EoSchemaFormComponent implements OnInit {
               },
               set: val => {
                 const conf = ifFields[key]?.[val]?.then;
-                if (val !== this.model[key]) {
+                if (isInit || val !== this.model[key]) {
                   if (conf?.properties) {
                     this.formatProperties({ ...configuration?.properties, ...conf.properties });
+                    isInit = false;
                   }
                   if (conf) {
                     this.initIfThenElse(conf);
