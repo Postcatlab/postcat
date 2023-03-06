230c9299fa0262e1212a793a8d24695e4afe80a2
buqiyuan <1743369777@qq.com>
2023-03-06 15:24:16 +0800

    fix: qiniu_env.js output path

--- scripts/deployWindows.js
+++ scripts/deployWindows.js
@@ -30,7 +30,7 @@ conn
             ...Array.from({ length: 5 }).map(_ => 'git pull'),
             'nvm install 16.19.1',
             'nvm use 16.19.1',
-            `echo ${process.env.QINIU_ENV_JS} > qiniu_env.js`,
+            `echo ${process.env.QINIU_ENV_JS} > ./scripts/qiniu_env.js`,
             'yarn install',
             'yarn release',
             'nvm use 12.22.10',
