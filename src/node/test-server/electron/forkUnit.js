648a8382bee18e1777d1bb33da3b16e603f32df8
scarqin <1054139596@qq.com>
2023-03-14 17:10:26 +0800

    feat: debug extension at node

--- src/node/test-server/electron/forkUnit.js
+++ src/node/test-server/electron/forkUnit.js
@@ -1,10 +1,9 @@
 let _LibsFlowCommon = require('../request/unit.js');
 let _LibsCommon = require('../request/libs/common.js');
-const { callErrorFromStatus } = require('@grpc/grpc-js/build/src/call.js');
 process.on('message', async message => {
   switch (message.action) {
     case 'setGlobal': {
-      console.log(message.datta);
+      global['__HOME_DIR'] = message.data.homeDir;
       break;
     }
     case 'ajax': {
