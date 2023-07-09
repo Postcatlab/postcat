a72c3b924c17c877646e02908fda2090eb13c816
scarqin <1054139596@qq.com>
2023-07-09 15:07:36 +0800

    test: update testcase

--- src/browser/src/app/services/extensions/webExtension.service.ts
+++ src/browser/src/app/services/extensions/webExtension.service.ts
@@ -36,7 +36,6 @@ export class WebExtensionService {
   constructor(private web: WebService, private language: LanguageService, private extensionCommon: ExtensionCommonService) {
     const isDevEnv = !APP_CONFIG.production || this.web.isVercel || 'http://52.76.76.88:8080'.includes(window.location.hostname);
     this.debugExtensionNames = isDevEnv && this.web.isWeb ? [] : [];
-    console.log(this.installedList);
   }
   async installExtension(extName: string, { version = 'latest' }) {
     //Get package.json
