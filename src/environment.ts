3eb090f0495d319b25136493a9643b6a38983310
“1626216058@qq.com” <1626216058@qq.com>
2024-11-04 20:39:16 +0800

    lab3

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,5 @@
-const variables = {
-  GITHUB_ORANIZATION_NAME: 'postcatlab',
-  GITHUB_PROJECT_NAME: 'postcat'
-};
+const GITHUB_ORGANIZATION_NAME = 'postcatlab';
+const GITHUB_PROJECT_NAME = 'postcat';
 
 export const COMMON_APP_CONFIG = {
   // EXTENSION_URL: 'http://localhost:5000',
@@ -10,8 +8,8 @@ export const COMMON_APP_CONFIG = {
   // SOCKET_PORT: '',
   // MOCK_URL: 'http://8.219.85.124:5000',
   NODE_SERVER_PORT: 4201,
-  GITHUB_ORANIZATION_NAME: variables.GITHUB_ORANIZATION_NAME,
-  GITHUB_PROJECT_NAME: variables.GITHUB_PROJECT_NAME,
+  GITHUB_ORANIZATION_NAME,
+  GITHUB_PROJECT_NAME,
   GITHUB_REPO_URL: `https://github.com/${variables.GITHUB_ORANIZATION_NAME}/${variables.GITHUB_PROJECT_NAME}`,
   BASE_DOWNLOAD_URL: 'https://data.postcat.com/download/'
   // BASE_DOWNLOAD_URL: 'http://127.0.0.1:8080'
